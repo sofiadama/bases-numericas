c5bfe330e3ffce91754a6dcf3c29d6b2106e4a4d
SOFIA <sofiadamasin@gmail.com>
2024-09-30 21:35:29 -0300

    Create decimal_para_binario.py

---
+++ decimal_para_binario.py
@@ -0,0 +1,13 @@
+num = int(input('Número: '))
+
+lista_bits = []
+
+while num > 0:
+    resto = num % 2
+    lista_bits.append(resto)
+    num = num // 2
+# '//' arredonda para o inteiro mais proximo
+
+lista_bits.reverse()
+
+print(lista_bits)  
